6ac04791ed33d6a517dc6ca7da9fe7a57bf32d31
Enej <enej.bacic@gmail.com>
2023-01-03 14:51:40 +0100

    cors 6

--- api/src/main/java/si/fri/rso/notifications/api/v1/NotificationApplication.java
+++ api/src/main/java/si/fri/rso/notifications/api/v1/NotificationApplication.java
@@ -10,13 +10,14 @@ import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-@CrossOrigin
+
 
 @OpenAPIDefinition(info = @Info(title = "Notifications API", version = "v1",
         contact = @Contact(email = "rso@fri.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing notifications."),
         servers = @Server(url = "http://localhost:8080/"))
 @ApplicationPath("/v1")
+@CrossOrigin
 public class NotificationApplication extends Application {
 
 }
