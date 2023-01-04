433a216a51348fdbdf418b32bd1e5fec7db3b13d
Enej <enej.bacic@gmail.com>
2023-01-04 20:24:20 +0100

    Test deleted insert into metadata table

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,2 +1,1 @@
-INSERT INTO image_metadata (title, description, width, height, created, uri) VALUES ('Our first image', 'This is the first image that we created for the RSO course.', 1280, 1325, TIMESTAMP '2006-01-01 15:36:38', 'https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/tess_first_light-tb.jpg');
 INSERT INTO notifications (title, description, created) VALUES ('Prvo obvestilo', 'To je moja prva mikrostoritev, narejena je za beleženje obvestil.', TIMESTAMP '2022-03-01 15:36:38');
